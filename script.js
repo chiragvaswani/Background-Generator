862ca3bde1c3cb266a132153eef9b896ee66234b
chiragvaswani <chiragvaswani751@gmail.com>
2021-03-11 16:58:41 +0530

    Created the setRandomGradient function.

--- script.js
+++ script.js
@@ -9,11 +9,20 @@ function setGradient() {
   body.style.background =
     "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
+  console.log(body.style.background);
   css.textContent = body.style.background + ";";
 }
 
 function setRandomGradient() {
-  body.style.background = "";
+  body.style.background =
+    "linear-gradient(to right, " +
+    String(Math.floor(Math.random() * 255)) +
+    ", " +
+    String(Math.floor(Math.random() * 255)) +
+    ")";
+  console.log(body.style.background);
+
+  css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
